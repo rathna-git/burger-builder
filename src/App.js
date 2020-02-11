a44bbd0fbe9f599919311d69236edaf9afa0a36b
Rathna Ganjigunta <rathna.ganjigunta@gmail.com>
2020-02-10 19:13:22 -0800

    created Layout component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Layout from './components/Layout/Layout';
 
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
